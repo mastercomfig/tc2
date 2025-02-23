e7bb6e9d6faef187efa87186cc85481034754a9a
ReplayCoding <replaycoding@gmail.com>
2025-02-23 00:47:54 -0700

    add support for linux to copy.sh

--- game_clean/copy.sh
+++ game_clean/copy.sh
@@ -13,31 +13,44 @@ rm -rf ${CLEAN_DEBUG_DIR}
 mkdir -p ${CLEAN_DIR}/{bin/$PLAT_DIR,tc2/bin/$PLAT_DIR,tc2/materials,tc2/cfg,tc2/scripts}
 mkdir -p ${CLEAN_DEBUG_DIR}/{bin/$PLAT_DIR,tc2/bin/$PLAT_DIR}
 
-declare -a EXES=(
-  tc2_win64
-  bin/$PLAT_DIR/captioncompiler
-  bin/$PLAT_DIR/glview
-  bin/$PLAT_DIR/height2normal
-  bin/$PLAT_DIR/motionmapper
-  bin/$PLAT_DIR/qc_eyes
-  bin/$PLAT_DIR/tgadiff
-  bin/$PLAT_DIR/vbsp
-  bin/$PLAT_DIR/vice
-  bin/$PLAT_DIR/vrad
-  bin/$PLAT_DIR/vtf2tga
-  bin/$PLAT_DIR/vtfdiff
-  bin/$PLAT_DIR/vvis
-)
-
 declare -a DLLS=(
-  bin/$PLAT_DIR/vrad_dll
-  bin/$PLAT_DIR/vvis_dll
   tc2/bin/$PLAT_DIR/{client,server}
 )
 
-declare -a DLLS_LIB=(
-  bin/$PLAT_DIR/steam_api64
-)
+if [ $PLATFORM = "win" ]; then
+  declare -a EXES=(
+    tc2_win64
+    bin/$PLAT_DIR/captioncompiler
+    bin/$PLAT_DIR/glview
+    bin/$PLAT_DIR/height2normal
+    bin/$PLAT_DIR/motionmapper
+    bin/$PLAT_DIR/qc_eyes
+    bin/$PLAT_DIR/tgadiff
+    bin/$PLAT_DIR/vbsp
+    bin/$PLAT_DIR/vice
+    bin/$PLAT_DIR/vrad
+    bin/$PLAT_DIR/vtf2tga
+    bin/$PLAT_DIR/vtfdiff
+    bin/$PLAT_DIR/vvis
+  )
+
+  DLLS+=(
+    bin/$PLAT_DIR/vrad_dll
+    bin/$PLAT_DIR/vvis_dll
+  )
+
+  declare -a DLLS_LIB=(
+    bin/$PLAT_DIR/steam_api64
+  )
+elif [ $PLATFORM = "linux" ]; then
+  declare -a EXES=(
+    tc2_linux64
+  )
+
+  declare -a DLLS_LIB=(
+    bin/$PLAT_DIR/libsteam_api
+  )
+fi
 
 declare -a FILES_REP=(
   tc2/cfg/valve.rc
@@ -67,7 +80,15 @@ done
 for F in "${DLLS[@]}"; do
   DLL=${F}${DLL_EXT}
   cp -f ${DEV_DIR}/${DLL} ${CLEAN_DIR}/${DLL}
-  cp -f ${DEV_DIR}/${F,,}.pdb ${CLEAN_DEBUG_DIR}/${F,,}.pdb
+  if [ $PLATFORM = "win" ]; then
+    cp -f ${DEV_DIR}/${F,,}.pdb ${CLEAN_DEBUG_DIR}/${F,,}.pdb
+  elif [ $PLATFORM = "linux" ]; then
+    # Linux binaries aren't stripped by the build scripts, so separate the
+    # debug info and strip them here.
+    cp -f ${CLEAN_DIR}/${DLL} ${CLEAN_DEBUG_DIR}/${DLL}.dbg
+    objcopy --add-gnu-debuglink=${CLEAN_DEBUG_DIR}/${DLL}.dbg ${CLEAN_DIR}/${DLL}
+    strip ${CLEAN_DIR}/${DLL}
+  fi
 done
 
 for F in "${DLLS_LIB[@]}"; do
