e02b75fad166dc0c9c450ad7ab3c60ffcc311797
mastercoms <mastercoms@tuta.io>
2025-02-22 20:46:49 -0500

    perf: add animation attachment deferral
    this skips a pre-mature/extra bone setup during particle simulation
    and flexing by allowing for the last frame's attachment position to be used
    
    technically this is a bit of a hack but it works well from my analysis.
    
    it gets rid of almost all of the particle cost in team fights besides sprite rendering

--- src/game/client/c_baseanimating.cpp
+++ src/game/client/c_baseanimating.cpp
@@ -1092,6 +1092,7 @@ CStudioHdr *C_BaseAnimating::OnNewModel()
 		}
 	}
 	m_BoneAccessor.Init( this, m_CachedBoneData.Base() ); // Always call this in case the studiohdr_t has changed.
+	m_iAccumulatedBoneMask = 0; // Reset the accumulated bone mask.
 
 	// Free any IK data
 	if (m_pIk)
@@ -2029,18 +2030,24 @@ bool C_BaseAnimating::PutAttachment( int number, const matrix3x4_t &attachmentTo
 		return false;
 
 	CAttachmentData *pAtt = &m_Attachments[number-1];
-	if ( gpGlobals->frametime > 0 && pAtt->m_nLastFramecount > 0 && pAtt->m_nLastFramecount == gpGlobals->framecount - 1 )
+	if ( gpGlobals->frametime > 0 && pAtt->m_nLastFramecount > 0 && pAtt->m_nLastFramecount < gpGlobals->framecount )
 	{
 		Vector vecPreviousOrigin, vecOrigin;
 		MatrixPosition( pAtt->m_AttachmentToWorld, vecPreviousOrigin );
 		MatrixPosition( attachmentToWorld, vecOrigin );
-		pAtt->m_vOriginVelocity = (vecOrigin - vecPreviousOrigin) / gpGlobals->frametime;
+		// compensate for the fact that the previous origin could have been multiple frames behind
+		pAtt->m_vOriginVelocity = (vecOrigin - vecPreviousOrigin) / (gpGlobals->frametime * (gpGlobals->framecount - pAtt->m_nLastFramecount));
+		// only update the frame count if the position changed, so we don't have to recompute attachments
+		if ( !pAtt->m_vOriginVelocity.IsZero(0.00001f) )
+		{
+			pAtt->m_nLastFramecount = gpGlobals->framecount;
+		}
 	}
 	else
 	{
 		pAtt->m_vOriginVelocity.Init();
+		pAtt->m_nLastFramecount = gpGlobals->framecount;
 	}
-	pAtt->m_nLastFramecount = gpGlobals->framecount;
 	pAtt->m_bAnglesComputed = false;
 	pAtt->m_AttachmentToWorld = attachmentToWorld;
 
@@ -2051,6 +2058,20 @@ bool C_BaseAnimating::PutAttachment( int number, const matrix3x4_t &attachmentTo
 	return true;
 }
 
+bool C_BaseAnimating::GetAttachmentDeferred( int number, matrix3x4_t& matrix )
+{
+	if (number < 1 || number > m_Attachments.Count())
+		return false;
+
+	// allow visual effects (eg. particles) to be a frame behind bone setup so that there are not messy dependencies.
+	CAttachmentData* pAtt = &m_Attachments[number - 1];
+	const bool bShouldUpdate = pAtt->m_nLastFramecount < gpGlobals->framecount - 1;
+	if ( bShouldUpdate && !CalcAttachments() )
+		return false;
+
+	matrix = pAtt->m_AttachmentToWorld;
+	return true;
+}
 
 bool C_BaseAnimating::SetupBones_AttachmentHelper( CStudioHdr *hdr )
 {
@@ -2896,7 +2917,9 @@ bool C_BaseAnimating::SetupBones( matrix3x4_t *pBoneToWorldOut, int nMaxBones, i
 			m_flLastBoneSetupTime = currentTime;
 		}
 		m_iPrevBoneMask = m_iAccumulatedBoneMask;
-		m_iAccumulatedBoneMask = 0;
+		// Keep record of the fact that we've used attachments. Because of deferred attachments, we can't keep track from the previous frame.
+		//m_iAccumulatedBoneMask = 0;
+		m_iAccumulatedBoneMask = m_iAccumulatedBoneMask & BONE_USED_BY_ATTACHMENT;
 
 #ifdef STUDIO_ENABLE_PERF_COUNTERS
 		CStudioHdr *hdr = GetModelPtr();
