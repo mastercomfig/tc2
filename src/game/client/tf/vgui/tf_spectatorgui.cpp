0d810842573584344a9eec277bc20bbabc8e68a8
mastercoms <mastercoms@tuta.io>
2025-08-03 22:52:43 -0400

    fix specgui crash

--- src/game/client/tf/vgui/tf_spectatorgui.cpp
+++ src/game/client/tf/vgui/tf_spectatorgui.cpp
@@ -1068,8 +1068,8 @@ bool CTFSpectatorGUI::InTournamentGUI( void )
 	}
 
 	// don't use specgui if we're using match HUD, or if we're in matchmaking
-	const bool bInTournament = TFGameRules()->IsInTournamentMode();
-	const bool bNoConflicts = !TFGameRules()->IsCompetitiveMode() && !TFGameRules()->IsEmulatingMatch() && !ShouldUseMatchHUD();
+	const bool bInTournament = TFGameRules() && TFGameRules()->IsInTournamentMode();
+	const bool bNoConflicts = TFGameRules() && !TFGameRules()->IsCompetitiveMode() && !TFGameRules()->IsEmulatingMatch() && !ShouldUseMatchHUD();
 
 	C_TFPlayer *pPlayer = C_TFPlayer::GetLocalTFPlayer();
 	const bool bIsSpectator = !pPlayer || pPlayer->IsHLTV() || ( pPlayer->GetTeamNumber() != TF_TEAM_RED && pPlayer->GetTeamNumber() != TF_TEAM_BLUE );
