676b9a48de9ab649133a3dfd8b6b555c81d043e5
mastercoms <mastercoms@tuta.io>
2025-08-03 21:35:17 -0400

    fix diamondback revert

--- src/game/shared/tf/tf_weaponbase.cpp
+++ src/game/shared/tf/tf_weaponbase.cpp
@@ -6585,7 +6585,7 @@ bool CTFWeaponBase::CanBeCritBoosted( void )
 
 bool CTFWeaponBase::CanHaveRevengeCrits( void )
 {
-#if !(defined(MCOMS_BALANCE_PACK) || 1)
+#if !(defined(MCOMS_BALANCE_PACK) || 0)
 	int iSapperCrits = 0;
 	CALL_ATTRIB_HOOK_INT( iSapperCrits, sapper_kills_collect_crits );
 	if ( iSapperCrits != 0 )
