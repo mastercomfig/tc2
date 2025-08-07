f58ebaab672e4951998adf819a2012fb421cb600
Coffee Bean <compositedynamic@gmail.com>
2025-08-07 16:34:27 +0000

    Workaround for CTFStatPanel not hiding on freezecam screenshot

--- src/game/client/tf/tf_hud_freezepanel.cpp
+++ src/game/client/tf/tf_hud_freezepanel.cpp
@@ -1126,6 +1126,11 @@ int	CTFFreezePanel::HudElementKeyInput( int down, ButtonCode_t keynum, const cha
 				engine->ClientCmd( "extendfreeze" );
 				view->FreezeFrame( TF_DEATH_ANIMATION_TIME );
 
+				// Hide StatPanel here as a workaround for not hiding in time for freezecam screenshots
+				CTFStatPanel* pStatPanel = GET_HUDELEMENT(CTFStatPanel);
+				if (pStatPanel && pStatPanel->IsVisible())
+					pStatPanel->SetVisible(false);
+
 				//Hide the reminder panel
 				m_flShowSnapshotReminderAt = 0;
 				ShowSnapshotPanel( false );
