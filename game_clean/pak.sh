251771df3018c916553ffc789e9be7ce952dcdbb
mastercoms <mastercoms@tuta.io>
2025-08-26 12:20:05 -0400

    full pak generation

--- game_clean/pak.sh
+++ game_clean/pak.sh
@@ -20,8 +20,12 @@ else
   exit 1
 fi
 
+FULL_BASE_DIR=$(cd .. && pwd -W)
+
+rm -f ../game/tc2/pak1*.vpk
+
 # TODO: do we want to use Steam Pipe and VDF manifest?
-vpk -M -k mcoms.publickey.vdf -K mcoms.privatekey.vdf ../game_src/tc2/pak1
+vpk -M -k mcoms.publickey.vdf -K mcoms.privatekey.vdf "${FULL_BASE_DIR}/game_src/tc2/pak1"
 
 # move instead of copy to regenerate multi-folder each time
 mv ../game_src/tc2/pak1*.vpk ../game/tc2/
