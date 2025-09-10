9cdbae0aeee9cd3d899f524f23175cfdb01d3f4a
mastercoms <mastercoms@tuta.io>
2025-09-10 10:49:46 -0400

    restore cl_interpolate original flags

--- src/game/client/c_baseentity.cpp
+++ src/game/client/c_baseentity.cpp
@@ -3213,7 +3213,7 @@ void C_BaseEntity::Simulate()
 }
 
 // Defined in engine
-static ConVar cl_interpolate( "cl_interpolate", "1.0f", FCVAR_USERINFO );
+static ConVar cl_interpolate( "cl_interpolate", "1.0f", FCVAR_USERINFO | FCVAR_DEVELOPMENTONLY );
 
 // (static function)
 void C_BaseEntity::InterpolateServerEntities()
