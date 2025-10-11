c3ef46c5f621c4b6ea96f81a4a32736bf8408dbb
mastercoms <mastercoms@tuta.io>
2025-10-11 19:10:51 -0400

    gameinfo load support

--- game/start_dedicated_tc2.sh
+++ game/start_dedicated_tc2.sh
@@ -3,6 +3,13 @@
 script=$(readlink -f -- "$0")
 pushd "$(dirname -- "$script")" > /dev/null
 
+if [ ! -f tc2/gameinfo_client.txt ]; then
+    cp tc2/gameinfo.txt tc2/gameinfo_client.txt
+    cp tc2/gameinfo_server.txt tc2/gameinfo.txt
+fi
+
 ./tc2.sh -console -dedicated -gatherdedi +sv_pure 1 "$@"
+
+mv tc2/gameinfo_client.txt tc2/gameinfo.txt
 
 popd > /dev/null
