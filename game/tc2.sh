4fc86a18661a774021e66d2e787d6e5b836ede0f
mastercoms <mastercoms@tuta.io>
2025-10-12 10:44:02 -0400

    make it more friendly for containers

--- game/tc2.sh
+++ game/tc2.sh
@@ -5,16 +5,16 @@ pushd "$(dirname -- "$script")" > /dev/null
 
 # Launch the game under the steam runtime
 if [ -z $SLR_SNIPER_PATH ]; then
-  if [ -z $(command -v steam) ]; then
-    SLR_SNIPER_PATH="/home/$USER/.local/share/Steam/steamcmd/SteamLinuxRuntime_sniper/run"
-    if [ ! -f ${SLR_SNIPER_PATH} ]; then
-      echo "Run steamcmd +force_install_dir ./SteamLinuxRuntime_sniper +login anonymous +app_update 1628350 validate +quit, or define \$SLR_SNIPER_PATH to the location of Steam Linux Runtime 3.0 run path."
-      exit 1
-    fi
-  else
-    SLR_SNIPER_PATH="/home/$USER/.steam/steam/steamapps/common/SteamLinuxRuntime_sniper/run"
-    if [ ! -f ${SLR_SNIPER_PATH} ]; then
-      echo "Install Steam Linux Runtime (by running steam steam://install/1628350), or define \$SLR_SNIPER_PATH to the location of Steam Linux Runtime 3.0 run file."
+  SLR_SNIPER_PATH="/home/$USER/.steam/steam/steamapps/common/SteamLinuxRuntime_sniper/run"
+  if [ ! -f ${SLR_SNIPER_PATH} ]; then
+    if [ -z $(command -v steam) ]; then
+      SLR_SNIPER_PATH="/home/$USER/.local/share/Steam/steamcmd/SteamLinuxRuntime_sniper/run"
+      if [ ! -f ${SLR_SNIPER_PATH} ]; then
+        echo "Run steamcmd +force_install_dir ./SteamLinuxRuntime_sniper +login anonymous +app_update 1628350 validate +quit, or define \$SLR_SNIPER_PATH to the location of the Steam Linux Runtime 3.0 run path."
+        exit 1
+      fi
+    else
+      echo "Install Steam Linux Runtime (by running steam steam://install/1628350), or define \$SLR_SNIPER_PATH to the location of the Steam Linux Runtime 3.0 run file."
       exit 1
     fi
   fi
