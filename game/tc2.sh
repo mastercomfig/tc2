c8b11b42b4d48da8b1c58a91e8b97a29bf0056cb
mastercoms <mastercoms@tuta.io>
2025-10-12 12:09:47 -0400

    use $HOME instead of assuming /home

--- game/tc2.sh
+++ game/tc2.sh
@@ -5,10 +5,10 @@ pushd "$(dirname -- "$script")" > /dev/null
 
 # Launch the game under the steam runtime
 if [ -z $SLR_SNIPER_PATH ]; then
-  SLR_SNIPER_PATH="/home/$USER/.steam/steam/steamapps/common/SteamLinuxRuntime_sniper/run"
+  SLR_SNIPER_PATH="$HOME/.steam/steam/steamapps/common/SteamLinuxRuntime_sniper/run"
   if [ ! -f ${SLR_SNIPER_PATH} ]; then
     if [ -z $(command -v steam) ]; then
-      SLR_SNIPER_PATH="/home/$USER/.local/share/Steam/steamcmd/SteamLinuxRuntime_sniper/run"
+      SLR_SNIPER_PATH="$HOME/.local/share/Steam/steamcmd/SteamLinuxRuntime_sniper/run"
       if [ ! -f ${SLR_SNIPER_PATH} ]; then
         echo "Run steamcmd +force_install_dir ./SteamLinuxRuntime_sniper +login anonymous +app_update 1628350 validate +quit, or define \$SLR_SNIPER_PATH to the location of the Steam Linux Runtime 3.0 run path."
         exit 1
