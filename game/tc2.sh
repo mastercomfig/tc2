5c5b31e0b9914b77d12869f35521c3ee5f7fda5d
mcoms <mastercoms@tuta.io>
2025-10-12 06:48:50 -0400

    SLR3

--- game/tc2.sh
+++ game/tc2.sh
@@ -4,6 +4,20 @@ script=$(readlink -f -- "$0")
 pushd "$(dirname -- "$script")" > /dev/null
 
 # Launch the game under the steam runtime
-/home/$USER/.steam/bin32/steam-runtime/run.sh ./tc2_linux64 -steam -particles 1 +ip 127.0.0.1 "$@"
+if [ -z $SLR_SNIPER_PATH ]; then
+  if [ -z $(command -v steam) ]; then
+    SLR_SNIPER_PATH="/home/$USER/.local/share/Steam/steamcmd/SteamLinuxRuntime_sniper/run"
+    if [ ! -f ${SLR_SNIPER_PATH} ]; then
+      echo "Run steamcmd +force_install_dir ./SteamLinuxRuntime_sniper +login anonymous +app_update 1628350 validate +quit, or define $SLR_SNIPER_PATH to the location of Steam Linux Runtime 3.0 run path."
+    fi
+  else
+    SLR_SNIPER_PATH="/home/$USER/.steam/steamapps/SteamLinuxRuntime_sniper/run"
+    if [ ! -f ${SLR_SNIPER_PATH} ]; then
+      echo "Install Steam Linux Runtime (by running steam steam://install/1628350), or define $SLR_SNIPER_PATH to the location of Steam Linux Runtime 3.0 run file."
+    fi
+  fi
+fi
+
+${SLR_SNIPER_PATH} --devel -- ./tc2_linux64 -steam -particles 1 +ip 127.0.0.1 "$@"
 
 popd > /dev/null
