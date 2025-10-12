b4a32f6ff21940f3900f0cbd71b9dd3cc1ad89e8
mcoms <mastercoms@tuta.io>
2025-10-12 09:52:28 -0400

    fix steam type

--- game/tc2.sh
+++ game/tc2.sh
@@ -11,7 +11,7 @@ if [ -z $SLR_SNIPER_PATH ]; then
       echo "Run steamcmd +force_install_dir ./SteamLinuxRuntime_sniper +login anonymous +app_update 1628350 validate +quit, or define $SLR_SNIPER_PATH to the location of Steam Linux Runtime 3.0 run path."
     fi
   else
-    SLR_SNIPER_PATH="/home/$USER/.steam/steamapps/SteamLinuxRuntime_sniper/run"
+    SLR_SNIPER_PATH="/home/$USER/.steam/steam/steamapps/SteamLinuxRuntime_sniper/run"
     if [ ! -f ${SLR_SNIPER_PATH} ]; then
       echo "Install Steam Linux Runtime (by running steam steam://install/1628350), or define $SLR_SNIPER_PATH to the location of Steam Linux Runtime 3.0 run file."
     fi
