4ecf0605bd8240b8da0fb70e0d1412cae29d439e
mastercoms <mastercoms@tuta.io>
2025-10-23 22:36:40 -0400

    remove unnecessary hack

--- src/game/client/tf/vgui/tf_playermodelpanel.cpp
+++ src/game/client/tf/vgui/tf_playermodelpanel.cpp
@@ -2478,7 +2478,6 @@ void CTFPlayerModelPanel::SetupFlexWeights( void )
 		}
 
 		m_flSceneTime += (m_RootMDL.m_MDL.m_flTime - m_flLastTickTime);
-		m_flSceneTime = Max( m_flSceneTime, -SCENE_LERP_TIME );
 		m_flLastTickTime = m_RootMDL.m_MDL.m_flTime;
 
 		if ( m_flSceneEndTime > FLT_EPSILON && m_flSceneTime > m_flSceneEndTime )
