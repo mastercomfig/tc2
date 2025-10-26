4d7e01e4eb5a119c8f3869399f7b01376649661b
mcoms <mastercoms@tuta.io>
2025-10-26 05:40:18 -0400

    symlink system libtinfo.so.6 to local libtinfo.so.5 for dedi to pick up :)

--- game/start_dedicated_tc2.sh
+++ game/start_dedicated_tc2.sh
@@ -8,6 +8,10 @@ if [ ! -f tc2/gameinfo_client.txt ]; then
     cp tc2/gameinfo_server.txt tc2/gameinfo.txt
 fi
 
+if [ ! -f bin/linux64/libtinfo.so.5 ]; then
+    ln -s /lib/x86_64-linux-gnu/libtinfo.so.6 bin/linux64/libtinfo.so.5
+fi
+
 ./tc2.sh -console -dedicated -gatherdedi +sv_pure 1 "$@"
 
 mv tc2/gameinfo_client.txt tc2/gameinfo.txt
