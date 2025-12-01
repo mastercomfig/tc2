be97206c550cd90613fe2c4da9b1de64729b97fe
mastercoms <mastercoms@tuta.io>
2025-12-01 01:26:27 -0500

    the brute force method

--- game_clean/shared.sh
+++ game_clean/shared.sh
@@ -4,7 +4,16 @@ DEV_DIR=../game
 CLEAN_DIR=../game_dist
 CLEAN_DEBUG_DIR=${CLEAN_DIR}_debug
 
-command -v 7z > /dev/null && CMD_7Z=7z || CMD_7Z=7za
+if [ command -v 7zz > /dev/null ]; then
+  CMD_7Z=7zz
+elif [ command -v 7z > /dev/null ]; then
+  CMD_7Z=7z
+elif [ command -v 7za > /dev/null ]; then
+  CMD_7Z=7za
+else
+  echo "7zz/7z/7za command not found! Please install 7zip."
+  exit 1
+fi
 
 if [[ "$OSTYPE" == "linux"* ]]; then
   PLATFORM="linux"
