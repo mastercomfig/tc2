4f6746f961297c6f569be892ae9b39fe9e211839
mastercoms <mastercoms@tuta.io>
2025-12-03 14:40:00 -0500

    use new release URL

--- game/update_dedicated.sh
+++ game/update_dedicated.sh
@@ -3,7 +3,7 @@
 script=$(readlink -f -- "$0")
 pushd "$(dirname -- "$script")" > /dev/null
 
-curl -Lo game.zip https://github.com/mastercomfig/tc2/releases/download/rel/game-linux.zip
+curl -Lo game.zip https://github.com/mastercomfig/tc2/releases/latest/download/game-linux.zip
 7z x game.zip
 rm game.zip
 
