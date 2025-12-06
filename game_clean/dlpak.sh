fa75be453ae83007011f817f6463c3a40950eb2e
mastercoms <mastercoms@tuta.io>
2025-12-06 00:14:21 -0500

    check pak version in dlpak

--- game_clean/dlpak.sh
+++ game_clean/dlpak.sh
@@ -8,7 +8,11 @@ set -e
 
 source ./shared.sh
 
-if [ -f "${DEV_DIR}/tc2/pak1_dir.vpk" ]; then
+PAK_VERSION_FILE="${DEV_DIR}/version.txt"
+
+CUR_PAK_VERSION=$((cat $PAK_VERSION_FILE || echo "") | tr -d '\n')
+
+if [[ -f "${DEV_DIR}/tc2/pak1_dir.vpk" && $CUR_PAK_VERSION == $PAK_VERSION ]]; then
   echo "pak already downloaded"
   exit 0
 fi
@@ -17,3 +21,5 @@ rm -f pak.zip
 curl -L "https://github.com/mastercomfig/tc2-pak/releases/download/$PAK_VERSION/pak.zip" -o pak.zip
 
 $CMD_7Z e pak.zip -y "-o${DEV_DIR}/tc2"
+
+echo $PAK_VERSION > $PAK_VERSION_FILE
