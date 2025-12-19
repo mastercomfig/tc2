fa1bd97196ed02225a8b4fb932c254262b33c331
mastercoms <mastercoms@tuta.io>
2025-12-19 18:29:09 -0500

    block use of hwm

--- src/game/shared/baseplayer_shared.cpp
+++ src/game/shared/baseplayer_shared.cpp
@@ -88,12 +88,12 @@
 #endif
 
 #ifdef CLIENT_DLL
-ConVar mp_usehwmmodels( "mp_usehwmmodels", "0", NULL, "Enable the use of the hw morph models. (-1 = never, 1 = always, 0 = based upon GPU)" ); // -1 = never, 0 = if hasfastvertextextures, 1 = always
+ConVar mp_usehwmmodels( "mp_usehwmmodels", "-1", NULL, "Enable the use of the hw morph models. (-1 = never, 1 = always, 0 = based upon GPU)" ); // -1 = never, 0 = if hasfastvertextextures, 1 = always
 #endif
 
 bool UseHWMorphModels()
 {
-#ifdef CLIENT_DLL 
+#if 0 && defined( CLIENT_DLL )
 	if ( mp_usehwmmodels.GetInt() == 0 )
 		return g_pMaterialSystemHardwareConfig->HasFastVertexTextures();
 
