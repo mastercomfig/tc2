c6f2dc60c9f80d13afa3861ce17141baef2fad63
mastercoms <mastercoms@tuta.io>
2026-02-04 21:23:33 -0500

    fix prompts on update_dedicated

--- game/update_dedicated.sh
+++ game/update_dedicated.sh
@@ -4,7 +4,7 @@ script=$(readlink -f -- "$0")
 pushd "$(dirname -- "$script")" > /dev/null
 
 curl -Lo game.zip https://github.com/mastercomfig/tc2/releases/latest/download/game-linux.zip
-7z x game.zip
+7z x -aoa -y game.zip
 rm game.zip
 
 popd > /dev/null
