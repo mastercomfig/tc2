875f96818447b8e954e3beb2f8c5862c2d402c60
mastercoms <mastercoms@tuta.io>
2026-03-13 09:44:14 -0400

    add support for -autoupdate

--- game/update_dedicated.sh
+++ game/update_dedicated.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 script=$(readlink -f -- "$0")
 pushd "$(dirname -- "$script")" > /dev/null
 
+./steamcmd_update.sh
 curl -Lo game.zip https://github.com/mastercomfig/tc2/releases/latest/download/game-linux.zip
 7z x -aoa -y game.zip
 rm game.zip
