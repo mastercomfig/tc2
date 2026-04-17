44de751692667f9943eee989635ce99ad2eb95c1
mastercoms <mastercoms@tuta.io>
2026-04-17 02:42:58 -0400

    add parameter to build custom shader list

--- src/materialsystem/stdshaders/buildsdkshaders.bat
+++ src/materialsystem/stdshaders/buildsdkshaders.bat
@@ -8,4 +8,10 @@ set SOURCE_DIR="..\..\"
 rem Change me to your mod's name!
 set GAME_DIR="..\..\..\game\tc2"
 
-%BUILD_SHADER% stdshader_dx9_30         -game %GAME_DIR% -source %SOURCE_DIR% -force30
+set SHADER_LIST=stdshader_dx9_30
+if "%1"=="" goto build
+
+set SHADER_LIST=%1
+
+:build
+%BUILD_SHADER% %SHADER_LIST% -game %GAME_DIR% -source %SOURCE_DIR% -force30
