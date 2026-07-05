688cff5a69176b9a74f82ebaecfb420e381b67cd
mastercoms <mastercoms@tuta.io>
2026-07-05 19:20:39 -0400

    remove spurious warning

--- src/game/server/tf/tf_gc_server.cpp
+++ src/game/server/tf/tf_gc_server.cpp
@@ -3150,7 +3150,7 @@ void CTFGCServerSystem::UpdateConnectedPlayersAndServerInfo( CMsgGameServerMatch
 			if ( pPlayerSteamID == NULL || !pPlayerSteamID->IsValid() )
 			{
 				// This can occur in lan-mode
-				Warning( "Player with no steam ID, counting as ad-hoc\n" );
+				//Warning( "Player with no steam ID, counting as ad-hoc\n" );
 			}
 
 			bActive = true;
