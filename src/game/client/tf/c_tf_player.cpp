mastercomfig/tc2

commit ff18f12631f3a3fdb2de4d80f898e94dc3564237

when flushing visibility state, also update attachment models

src/game/client/tf/c_tf_player.cpp
@@ -4827,12 +4827,6 @@ void C_TFPlayer::OnDataChanged( DataUpdateType_t updateType )
 			C_TFPlayer *pTFOldObserverTarget = ToTFPlayer( m_hOldObserverTarget.Get() );
 			if ( m_hOldObserverTarget != GetObserverTarget() && pTFOldObserverTarget )
 			{
-				C_TFWeaponBase *pWeapon = pTFOldObserverTarget->m_Shared.GetActiveTFWeapon();
-				if ( pWeapon )
-				{
-					pWeapon->UpdateAttachmentModels();
-				}
-
 				// Update visibility of any worn items.
 				pTFOldObserverTarget->FlushAllPlayerVisibilityState();
 
@@ -4845,12 +4839,6 @@ void C_TFPlayer::OnDataChanged( DataUpdateType_t updateType )
 			C_TFPlayer *pTFObserverTarget = ToTFPlayer( GetObserverTarget() );
 			if ( pTFObserverTarget )
 			{
-				C_TFWeaponBase *pWeapon = pTFObserverTarget->m_Shared.GetActiveTFWeapon();
-				if ( pWeapon )
-				{
-					pWeapon->UpdateAttachmentModels();
-				}
-
 				// Update visibility of any worn items.
 				pTFObserverTarget->FlushAllPlayerVisibilityState();
 			}
@@ -10389,14 +10377,8 @@ void C_TFPlayer::FlushAllPlayerVisibilityState()
 
 	// Update our viewmodel whenever we switch view modes
 	C_TFPlayer *pTFObserverTarget = ToTFPlayer( GetObserverTarget() );
-	if ( pTFObserverTarget )
+	if ( pTFObserverTarget && pTFObserverTarget != this )
 	{
-		C_TFWeaponBase *pWeapon = pTFObserverTarget->m_Shared.GetActiveTFWeapon();
-		if ( pWeapon )
-		{
-			pWeapon->UpdateAttachmentModels();
-		}
-
 		pTFObserverTarget->FlushAllPlayerVisibilityState();
 	}
 
@@ -10405,6 +10387,7 @@ void C_TFPlayer::FlushAllPlayerVisibilityState()
 	if ( pWeapon )
 	{
 		pWeapon->UpdateModelIndex();
+		pWeapon->UpdateAttachmentModels();
 		pWeapon->UpdateVisibility();
 	}
 
@@ -11526,12 +11509,6 @@ void C_TFPlayer::FireGameEvent( IGameEvent *event )
 
 			if ( iOld == OBS_MODE_IN_EYE || iNew == OBS_MODE_IN_EYE )
 			{
-				C_TFWeaponBase *pWeapon = m_Shared.GetActiveTFWeapon();
-				if ( pWeapon )
-				{
-					pWeapon->UpdateAttachmentModels();
-				}
-
 				// Update visibility of any worn items.
 				FlushAllPlayerVisibilityState();
 			}
@@ -11544,15 +11521,6 @@ void C_TFPlayer::FireGameEvent( IGameEvent *event )
 		if ( iTarget == entindex() || iOldTarget == entindex() )
 		{
 			int iMode = event->GetInt( "mode" );
-			if ( iMode == OBS_MODE_IN_EYE )
-			{
-				C_TFWeaponBase *pWeapon = m_Shared.GetActiveTFWeapon();
-				if ( pWeapon )
-				{
-					pWeapon->UpdateAttachmentModels();
-				}
-			}
-
 			// Update visibility of any worn items.
 			FlushAllPlayerVisibilityState();
 		}

commit 12dcd0b246f72367e0460349f3e34b1ad7a2273a

fix invis weapons regression caused by ff18f12631f3a3fdb2de4d80f898e94dc3564237

src/game/client/tf/c_tf_player.cpp
@@ -10464,6 +10464,13 @@ void C_TFPlayer::ForceTempForceDraw( bool bThirdPerson )
 
 void C_TFPlayer::FlushAllPlayerVisibilityState()
 {
+	// TODO(mcoms): is this the best way to do this?
+	static CUtlVector<C_TFPlayer*> s_FlushingPlayers;
+	if ( s_FlushingPlayers.Find( this ) != s_FlushingPlayers.InvalidIndex() )
+		return;
+
+	s_FlushingPlayers.AddToTail( this );
+
 	// We've switch from first to third, or vice versa.
 	UpdateVisibility();
 
@@ -10488,6 +10495,13 @@ void C_TFPlayer::FlushAllPlayerVisibilityState()
 		}
 	}
 
+	// Update our viewmodel whenever we switch view modes
+	C_TFPlayer *pTFObserverTarget = ToTFPlayer( GetObserverTarget() );
+	if ( pTFObserverTarget && pTFObserverTarget != this )
+	{
+		pTFObserverTarget->FlushAllPlayerVisibilityState();
+	}
+
 	// Update our weapon's visibility when we switch
 	C_TFWeaponBase *pWeapon = GetActiveTFWeapon();
 	if ( pWeapon )
@@ -10500,6 +10514,8 @@ void C_TFPlayer::FlushAllPlayerVisibilityState()
 	// Update visibility of any worn items.
 	UpdateWearables();
 	SetBodygroupsDirty();
+
+	s_FlushingPlayers.FindAndRemove( this );
 }
 
 //-----------------------------------------------------------------------------